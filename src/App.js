f9c2119ad27401928b15edf392bfde8da8687e8f
amadi5892 <amadi5892@gmail.com>
2022-12-08 21:52:49 -0500

    Hello World

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      Hello World
+      <h2 className='app-title' >To-Do List</h2>
+      <div className='note-card-container' >
+        <h4>01</h4>
+        <form>
+          <label>Title</label>
+          <input type='text' placeholder='Enter title...' />
+          <label>Make a note</label>
+          <textarea type='text' placeholder='Note...' ></textarea>
+          <button>Save</button>
+        </form>
+      </div>
     </div>
   );
 }
